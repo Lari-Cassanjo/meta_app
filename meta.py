40c685d51e364478a843303e018516fdd4ef4e61
Larissa Cassanjo <larissacassanjo@gmail.com>
2024-07-12 19:29:42 -0300

    Terceiro objetivo concluído

--- meta.py
+++ meta.py
@@ -2,14 +2,26 @@ from os import system
 system('cls')
 
 paginas = []
+total_paginas = 0
 
 sair = 0
 print('Digite quantas páginas fez por processo (insira 999 para finalizar):')
 while sair != 999:
     sair = int(input(''))
     if sair != 999:
+        total_paginas += sair
         paginas.append(sair)
 processos = len(paginas)
 
-print(paginas)
-print(processos)
+print(f'Lista de páginas: {paginas}')
+print(f'Total de processos: {processos}')
+print(f'Total de páginas: {total_paginas}')
+
+if total_paginas >= 3000 or processos >= 160:
+    if total_paginas >= 3000:
+        print(f'\33[32mMeta de páginas batida com {total_paginas} páginas!\33[m')
+    if processos >= 160:
+        print(f'\33[32mMeta de processos batida com {processos} processos!\33[m')
+else:
+    print('\33[31mNenhuma meta foi alcaçada hoje.\33[m')
+    print(f'Faltaram \33[31m{3000 - total_paginas}\33[m páginas e \33[31m{160 - processos}\33[m processos.')
